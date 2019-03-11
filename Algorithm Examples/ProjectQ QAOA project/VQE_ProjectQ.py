be4da0616590b5ce764c2fc33d04804a21f2555a
wpol18 <William@williams-mbp.lan>
2019-03-11 17:50:43 -0400

    Added a run method to VQE. I think it's working...

--- Algorithm Examples/ProjectQ QAOA project/VQE_ProjectQ.py
+++ Algorithm Examples/ProjectQ QAOA project/VQE_ProjectQ.py
@@ -7,56 +7,148 @@ from projectq import MainEngine
 from projectq.ops import QubitOperator, Measure, All, Rx, Ry
 
 import numpy as np
+from scipy.optimize import minimize
+
 from collections import Counter
-import time
 
 class VQE(object):
+    '''
+    The Variational Quantum Eigensolver algorithm
 
-    def __init__(self):
-        ''' To-do... '''
+    VQE is an object that encapsulates the VQE algorithm (functional
+    minimization). The main components of the VQE algorithm are a minimizer
+    function for performing the functional minimization, a function that takes a
+    set of parameters to initialize an ansatz state, and a Hamiltonian of which
+    to calculate the expectation value.
+
+    Using this object:
+
+        1) initialize with `inst = VQE(minimizer)` where `minimizer` is a
+        function that performs a gradient free minimuzation--i.e.
+        scipy.optimize.minimize(., ., method='Nelder-Mead')
+
+        2) call `inst.run(ansatz, hamiltonian, initial_parameters)`. Returns
+        the optimal parameters and minimum expectation
+    '''
+    
+
+    def __init__(self, minimizer = minimize, minimizer_args = [], minimizer_kwargs = {'method': 'Nelder-Mead'}):
+        self.minimizer = minimizer
+        self.minimizer_args = minimizer_args
+        self.minimizer_kwargs = minimizer_kwargs
+
+        '''
+        Set up minimiser to be used in main VQE algorithm.
+        Default is scipy's optimize.minimize. 
+            
+        Parameters:
+            
+            :minimizer: function that minimizes objective f(obj, param). For
+            example the function scipy.optimize.minimize() needs at least two
+            parameters, the objective and an initial point for the optimization.
+            The args for minimizer are the cost function (provided by this class),
+            initial parameters (passed to run() method). kwargs can be passed in below.
+            
+            :minimizer_args: (list) arguments to be passed to `minimizer` in
+            form of *args (see `minimizer` above). Default = None
+            
+            :min_kwargs: (dict) arguments to be passed to `minimizer `in
+            form of **kwargs (see `minimizer` above). Default = {'method': 'Nelder-Mead'}
+            
+            '''
+
         
         
-    def run(self, ansatz, hamiltonian, shots = 10000, eng = None):
-        ''' Main function. '''
-        pass
+    def run(self, ansatz, hamiltonian, initial_params, shots = 10000, eng = None):
+        '''
+        Main function. Applies the VQE algorithm with the given parameters and arguments
+        using the minimizing function set up in self.__init__(). Finds the parameters
+        that create the best ansatz state with lowest possible expectation value for the
+        Hamiltonian.
+
+        Parameters:
+
+            :ansatz: (function: params, engine -> qureg) function that prepares a state to
+            use to calculate expectation value of `hamiltonian`.
+
+            :hamiltonian: (QubitOperator) the hamiltonian of which to calculate
+            the expectation value.
+
+            :initial_params: (list) initial parameters to provide to the minimizing function
+
+            :shots: (int) number of times to sample in calculating expectation
+
+            :eng: (MainEngine) the engine object we will be making calls to
+
+        '''
+
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # Check types
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        types = [QubitOperator, list, int]
+        args = [hamiltonian, initial_params, shots]
+        argstr = ['hamiltonian', 'initial_params', 'shots']
+
+        for Type, arg in zip(types, args):
+            if not isinstance(arg, Type):
+                raise TypeError('Argument `{}` provided to expectation must be of type {}'.format(arg, Type))
+            
+        if shots <= 0:
+            raise ValueError("`samples` variable must be a positive integer")
+
+        
+        if not callable(ansatz):
+            raise TypeError('Argument `ansatz` provided to expectation must be a function of two arguments - the state preparation parameters and a QC engine.') 
+    
+
+
+
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # Prepare Objective Function
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~        
+        def objective_function(params):
+            exp = self.expectation(lambda eng: ansatz(params, eng), hamiltonian, shots, eng)
+            
+            self._current_expectation = exp # store for printing
+            
+            return exp
+    
+
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # Using minimzer to finally find best expectation value and params
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        final_result = self.minimizer(objective_function, initial_params, *self.minimizer_args, **self.minimizer_kwargs)
+
+        return final_result
+
 
     
+
     def expectation(self, state_prep, hamiltonian, shots, eng):
         
         """
         Compute the expecation value of a given hamiltonian over the distribution
         generated by some given state_prep function.
+
+        Parameters:
         
-        :param state_prep: The state preparation function used to calculate the
-        expectation value.
         
-        :param hamiltonian: QubitOperator representing the hamiltonian of which
-        to calculate the expectation value.
+            :state_prep: The state preparation function used to calculate the
+            expectation value.
         
-        :param shots: The number of samples used to calculate the expectation value.
+            :hamiltonian: QubitOperator representing the hamiltonian of which
+            to calculate the expectation value.
         
-        :param eng: The MainEngine object we will be making calls to.
+            :shots: The number of samples used to calculate the expectation value.
         
-        :return: A float representing the expectation value of hamiltonian given
-        the state prepared by state_prep.
+            :eng: The MainEngine object we will be making calls to.
+        
+            :return: A float representing the expectation value of hamiltonian given
+            the state prepared by state_prep.
+            
         
         """
         
-        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        # Check types
-        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        if not isinstance(hamiltonian, QubitOperator):
-            raise TypeError('Argument `hamiltonian` provided to expectation must be a QubitOperator.')
-        
-        if not isinstance(shots, int):
-            raise TypeError('Argument `shots` provided to expectation must be an integer.')
-
-        if shots <= 0:
-            raise ValueError("samples variable must be a positive integer")
-
-        eng = eng or MainEngine()
-        if not isinstance(eng, MainEngine):
-            raise TypeError('Argument `eng` provided to expectation_naive must be a MainEngine.')
 
 
 
@@ -122,6 +214,7 @@ class VQE(object):
         # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         # Add or subtract from expectation value depending on number of 1's in bitstring
         # If number of 1s is odd, subtract from total expectation value. If even, add.
+        #
         # Reasoning is that reading out a |0 > corresponds to an eigenvalue of +1, and
         # reading out a |1 > corresponds to an eigenvalue of -1. We're multiplying the
         # eigenvalues of each individual term, so if we had an odd number of terms we'd
@@ -175,6 +268,7 @@ if __name__ == "__main__":
     ham_example2 = QubitOperator('X0 Z1', 3.0) + \
                            QubitOperator('Y1') + \
                            QubitOperator('Z1', 2)
+    ham_example3 = QubitOperator('Z0')
 
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -188,10 +282,57 @@ if __name__ == "__main__":
     # and ~ -5.0 if we use ham_example2
     # Let's check it out:
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    print('\nTesting expectation method. Should get -5.0...')
     start = time.time()
     exp = VQE().expectation(state_prep, ham_example2, 1000, eng_example)
     end = time.time()
     print('\nExpectation (calculated in', end - start, 'seconds):\n', exp, '\n')
+
+
+
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Let's test our run method
+    #
+    # First let's define some state_prep/ansatz functions 
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    def ansatz_1(params, eng):
+        qureg = eng.allocate_qureg(1)
+        Ry(params[0]) | qureg[0]
+        return qureg
+
+    def ansatz_2(params, eng):
+        qureg = eng.allocate_qureg(1)
+        Rx(params[0]) | qureg[0]
+        return qureg
+
+
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Run a couple of examples, instantiate a VQE object
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    vqe = VQE()
+    print('\nTesting run method using Ry...')
+    start = time.time()
+    result = vqe.run(ansatz_1, ham_example3, [3], shots = 1000, eng = eng_example)
+    end = time.time()
+    print('Optimal eigenvalue:', result['fun'], '\nOptimal parameter:', result['x'], '\n(Calculated in', end - start, 'seconds)')
+
+    print('\nTesting run method using Rx...')
+    start = time.time()
+    result = vqe.run(ansatz_2, ham_example3, [3], shots = 1000, eng = eng_example)
+    end = time.time()
+    print('Optimal eigenvalue:', result['fun'], '\nOptimal parameter:', result['x'], '\n(Calculated in', end - start, 'seconds)')
+    
+    
+
+        
+
+    
+    
+
+
+
 
 
 
